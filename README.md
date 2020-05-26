5139c42ed784d1beb4890135e229ee2a6084c2df
Jacob Macklin <bigmack81@gmail.com>
2020-05-26 12:08:19 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# project-1-mehrab
+# Project-1-JacobMacklin
+## Overview
+Parses a csv file of 2k+ rows of data into an Apache Spark RDD to be manipulated and provide useful information. Utilized through the use of a Tomcat server and persisted to a PostgreSQL database hosted on an EC2 instance.
+## Build
+### Java
+mvn clean compile package
+### Tomcat
+Run .war on tomcat
+### Docker
+docker built -t sparkdb .
+docker run -p 5432:5432 -d --name sparkdb sparkdb
+## Usage
+localhost:8080/spark-0.0.1-SNAPSHOT/spark?request=x
+### Where x =
+
+
--- src/main/java/spark/SparkServlet.java
+++ src/main/java/spark/SparkServlet.java
@@ -28,7 +28,6 @@ public class SparkServlet extends HttpServlet {
 	JavaSparkContext sparkContext;
 	ArrayList<String> names;
 	JavaRDD<Entry> entries;
-	// List<String> names;
 
 	@Override
 	public void init() throws ServletException {
@@ -36,27 +35,6 @@ public class SparkServlet extends HttpServlet {
 		sparkContext = new JavaSparkContext(conf);
 		JavaRDD<String> data = sparkContext.textFile("C:/Users/bigma/Documents/RevatureWork/Project-1-JacobMacklin/src/main/resources/resource.csv");
 		entries = data.map((f) -> EntryParser.parse(f));
-
-
-		/*
-		names = new ArrayList<>();
-		names.add("Mehrab");
-		names.add("Sutter");
-		names.add("Sutter");
-		names.add("Sutter");
-		names.add("Sutter");
-		names.add("Sutter");
-		names.add("Sutter");
-		names.add("Sutter");
-		names.add("Jeremy");
-		names.add("Jeremy");
-		names.add("Jeremy");
-		names.add("Daniel");
-		names.add("Daniel");
-		names.add("Daniel");
-		names.add("Daniel");
-		names.add("Daniel");
-		*/
 	}
 
 	@Override
@@ -105,47 +83,6 @@ public class SparkServlet extends HttpServlet {
 			}
 			
 		}
-
-		/*
-		String name = req.getParameter("name");
-		String lang = req.getParameter("lang");
-		if (name != null && lang != null) {
-			names.add(name);
-			if (lang.equals("en")) {
-				resp.getWriter().println(new HelloGreeter().greet(name));
-			} else if (lang.equals("es")) {
-				Greetable holaGreeter = new Greetable() {
-
-					@Override
-					public String greet(String name) {
-						return "Hola, " + name;
-					}
-				};
-				resp.getWriter().println(holaGreeter.greet(name));
-			} else if (lang.equals("de")) {
-				Greetable gutentagGreetable = (n) -> {
-					return "Guten tag, " + n;
-				};
-				resp.getWriter().println(gutentagGreetable.greet(name));
-				Runnable runner = () -> {
-					System.out.println("Running thread");
-				};
-				new Thread(runner).start();
-			}
-		}
-
-		for (String n : names) {
-			resp.getWriter().println(n);
-		}
-		names.stream().map((n) -> {
-			return n + "!";
-		}).forEach((n) -> {
-			System.out.println(n);
-		});
-		names.forEach((x) -> {
-			System.out.println(x);
-		});
-		*/
 	}
 
 	@Override
@@ -154,12 +91,6 @@ public class SparkServlet extends HttpServlet {
 		JavaPairRDD<String, Integer> test = entries.mapToPair((f) -> new Tuple2<>(f.getSubject(), 1));
 		JavaPairRDD<String, Integer> count = test.reduceByKey((x, y) -> (int) x + (int) y);
 		resp.getWriter().print(count.collect());
-		
-		//JavaRDD<String> namesRDD = sparkContext.parallelize(names);
-		//JavaPairRDD<String, Integer> namesMapper = namesRDD.mapToPair((f) -> new Tuple2<>(f, 1));
-		//resp.getWriter().println(namesMapper.collect());
-		//JavaPairRDD<String, Integer> countNames = namesMapper.reduceByKey((x, y) -> ((int) x + (int) y));
-		//resp.getWriter().println(countNames.collect());
 		
 	}
 }
